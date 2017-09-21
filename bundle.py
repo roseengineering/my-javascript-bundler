0db5ce1926697ad7ced671ce5e60e570af903117
george <george>
2017-09-21 15:02:01 -0400

    put back new line

--- bundle.py
+++ bundle.py
@@ -44,6 +44,7 @@ for filename in args:
         buf = f.read()
         sys.stdout.write("'%s':function(require, module, exports){\n" % norm(filename))
         sys.stdout.write(buf)
+        sys.stdout.write("\n")    # just in case the last light is a comment
         sys.stdout.write("},\n")
 
 sys.stdout.write("})('%s');\n" % entrypoint)
